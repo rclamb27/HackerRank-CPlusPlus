194e2223ce943c5a866a1e237e3c84d06d2081e1
Ryan Lamb <61068406+rclamb27@users.noreply.github.com>
2021-07-19 10:38:04 -0700

    Create input_output.cpp

---
+++ input_output.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, b, a; 
+    cin >> a >> b >> n;
+    cout << a+b+n; 
+    return 0;
+}
